6d59f537f41aff7a38de9d5d187afcbd07ea12f2
ult-shimizu-a <101547154+ult-shimizu-a@users.noreply.github.com>
2022-04-01 16:40:09 +0900

    HTMLのフォームを利用したwebサービス

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
 'use strict';
 const http = require('http');
 const server = http
-  .createServer((req, res) => {
-    const now = new Date();
-    console.info('[' + now + '] Requested by ' + req.socket.remoteAddress);
-    res.writeHead(200, {
-      'Content-Type': 'text/plain; charset=utf-8'
-    });
+.createServer((req, res) => {
+const fs = require('fs');
+  const now = new Date();
+  console.info('[' + now + '] Requested by ' + req.socket.remoteAddress);
+  res.writeHead(200, {
+    'Content-Type': 'text/html; charset=utf-8'
+  });
 
-    switch (req.method) {
-      case 'GET':
-        res.write('GET ' + req.url);
-        break;
-      case 'POST':
-        res.write('POST ' + req.url);
-        let rawData = '';
-        req
-          .on('data', chunk => {
-            rawData += chunk;
-          })
-          .on('end', () => {
-            console.info('[' + now + '] Data posted: ' + rawData);
-          });
-        break;
-      default:
-        break;
-    }
-    res.end();
-  })
+  switch (req.method) {
+    case 'GET':
+      const rs = fs.createReadStream('./form.html');
+      rs.pipe(res);
+      break;
+    case 'POST':
+      let rawData = '';
+      req
+        .on('data', chunk => {
+          rawData = rawData + chunk;
+        })
+        .on('end', () => {
+          const decoded = decodeURIComponent(rawData);
+          console.info('[' + now + '] 投稿: ' + decoded);
+          res.write(
+            '<!DOCTYPE html><html lang="ja"><body><h1>' +
+              decoded + 
+              'が投稿されました</h1></body></html>'
+          );
+          res.end();
+        });
+      break;
+    default:
+      break;
+   }
+})
   .on('error', e => {
     console.error('[' + new Date() + '] Server Error', e);
   })
